e703b41ecff56363c1491a55cea2c43eb1baf7e7
Renaud Bastien <rno.bstin@gmail.com>
2017-12-05 17:34:15 +0100

    some debug

--- RockSteady/droneControl.py
+++ RockSteady/droneControl.py
@@ -25,7 +25,7 @@ def generateSinFunction(xCrop):
     
     print('More Generation')
     print(X)
-    print(np.power(X,2))
+    print(np.power(X,2)+np.power(Y,2))
     R=np.power(X,2)+np.power(Y,2);
     print('More Generation')
     circle = R<.99
