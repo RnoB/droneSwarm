a6291e9d030a7d7afb64108e5b3f2ee809e7b31d
Renaud Bastien <rno.bstin@gmail.com>
2017-12-06 14:30:09 +0100

    debugging vision-motor output

--- RockSteady/camera361.py
+++ RockSteady/camera361.py
@@ -7,13 +7,13 @@ with picamera.PiCamera() as camera:
     camera.resolution = (1296,976)
     #camera.zoom = ( .3563 , 0.2875 , 228/640 , 228/480 )
     camera.framerate = 10
-    camera.iso = 400
+    camera.iso = 800
     camera.shutter_speed = 50000
     #camera.awb_mode = 'off'
     #camera.awb_gains=(1,1)
     #camera.exposure_speed = 100
     #camera.exposure_mode = 'night'
-    camera.exposure_compensation = 25
+    camera.exposure_compensation = 20
     camera.start_preview()
     firstRound = True
     
@@ -23,7 +23,7 @@ with picamera.PiCamera() as camera:
             print(filename)
             print('analog gain : '+str(camera.analog_gain)+' digital_gain : '+str(camera.digital_gain))
 
-            if camera.analog_gain >7 and camera.digital_gain > 1 and firstRound:
+            if camera.analog_gain >5 and camera.digital_gain > 1 and firstRound:
                 
                 camera.exposure_mode = 'off'
                 camera.awb_mode = 'off'
@@ -33,18 +33,18 @@ with picamera.PiCamera() as camera:
                 camera.awb_gains=(r,b)
                 firstRound = False
             if not firstRound:
-                r=r+1
+                r=r+.1
                 if r>8:
                     r=0
-                    b=b+1
+                    b=b+.1
                 if b>8:
                     break
                 camera.awb_gains=(r,b)
-                thershold=150
+                
             print(i)
             print('White balance gain : '+str(camera.awb_gains))
             #time.sleep(.1)
-            if i == 400:
+            if i == 40000:
                 break
     finally:
         camera.stop_preview()
