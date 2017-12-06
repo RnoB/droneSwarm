daa9ad64e825f31888d38c34b0548ab60e94420f
Renaud Bastien <rno.bstin@gmail.com>
2017-12-06 10:54:48 +0100

    debugging vision-motor output

--- RockSteady/droneControl.py
+++ RockSteady/droneControl.py
@@ -112,7 +112,7 @@ class visionAnalyzer(PiRGBAnalysis):
             print('opencv thresholding : '+str(t1-t0))
         t0=time.time()
         #frameC = cv2.bitwise_and(frameC,frameC,mask = self.circularMask)
-        ORANGE_MIN = np.array([150, 50, 50],np.uint8)
+        ORANGE_MIN = np.array([150, 100, 100],np.uint8)
         ORANGE_MAX = np.array([180, 255, 255],np.uint8)
 
         hsv_img = cv2.cvtColor(frameC,cv2.COLOR_BGR2HSV)
